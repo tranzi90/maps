85202c7ca612f6ba8ebba7bd6041166f57d13956
denis_levchenko <tranzi90@gmail.com>
2023-11-29 23:28:23 +0300

    add company model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hi there!');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+
+console.log(user);
+
+const company = new Company();
+console.log(company);
